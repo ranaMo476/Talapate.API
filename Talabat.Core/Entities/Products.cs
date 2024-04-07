17ebd046e695032a510186fd8097f60212579746
ranaMo476 <78308759+ranaMo476@users.noreply.github.com>
2024-04-07 14:19:40 +0200

    Add files via upload

---
+++ Talabat.Core/Entities/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Talabat.Core.Entities
+{
+    public class Products : BaseEntity 
+    {
+        
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string PictureUrl { get; set; }
+        public decimal Price { get; set; }
+
+        public int ProductsBrandId { get; set; }
+
+        public ProductsBrand ProductsBrand { get; set; }
+
+        public int ProductsTypeId { get; set; }
+
+        public ProductType ProductType { get; set; }
+
+    }
+}
